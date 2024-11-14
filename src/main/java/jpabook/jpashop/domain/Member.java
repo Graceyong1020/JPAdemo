e5724586703b3d36bea2845f45c6e0d847453385
GRACE\Baby <graceyong1020@gmail.com>
2024-11-14 07:22:23 -0800

    회원 등록, 수정, 조회 API

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,10 +16,13 @@ public class Member {
     @Id @GeneratedValue
     @Column(name = "member_id")
     private Long id;
+
+    @NotEmpty
     private String name;
     @Embedded // 내장 타입
     private Address address;
 
+    @JsonIgnore // 양방향 연관관계에서 한쪽을 JsonIgnore로 설정해야 한다.
     @OneToMany(mappedBy = "member")// 맴버와 오더는 일대다 관계
     private List<Order> orders = new ArrayList<>();
 
